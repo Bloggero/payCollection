d7927bbafc2b31b5e2d4acc410f1316d2dd972e7
Kevin <kevin.cano.pc@gmail.com>
2023-01-05 13:17:05 -0600

    ADD: CollectionController, model nad migration

--- database/migrations/2023_01_05_191549_create_collections_table.php
+++ database/migrations/2023_01_05_191549_create_collections_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('collection', function (Blueprint $table) {
+        Schema::create('collections', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id');
             $table->text('description')->nullable();
@@ -33,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('collection');
+        Schema::dropIfExists('collections');
     }
 };
