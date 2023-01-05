ea8daab25190a813d135aa49625c8811ab2ef8cb
Kevin <kevin.cano.pc@gmail.com>
2023-01-05 12:27:40 -0600

    UPDATE: dashboard view data has ben udpated

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,7 +7,75 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <button class="btn btn-primary float-right">New User</button>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-header">
+                         <b>Jose Luis</b>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                        <hr />
+                        <p class="card-text text-danger"><b>$1600</b></p>
+
+                    </div>
+
+                    <a href="#" class="card-link bg-success"><div class="card-footer text-center">
+                        Pay
+                    </div>
+                </a>
+                </div>
+
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-header">
+                         <b>Jose Luis</b>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                        <hr />
+                        <p class="card-text text-danger"><b>$1600</b></p>
+
+                    </div>
+
+                    <a href="#" class="card-link bg-success"><div class="card-footer text-center">
+                        Pay
+                    </div>
+                </a>
+                </div>
+
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-header">
+                         <b>Jose Luis</b>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                        <hr />
+                        <p class="card-text text-danger"><b>$1600</b></p>
+
+                    </div>
+
+                    <a href="#" class="card-link bg-success"><div class="card-footer text-center">
+                        Pay
+                    </div>
+                </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 @stop
 
 @section('css')
@@ -15,5 +83,7 @@
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+    <script>
+        console.log('Hi!');
+    </script>
 @stop
