e1474df9e156c1367779c520b33a0d8cf25a83c5
Kevin <kevin.cano.pc@gmail.com>
2023-01-22 16:31:39 -0600

    UPDATE: Statistics view

--- resources/views/admin/statistics.blade.php
+++ resources/views/admin/statistics.blade.php
@@ -10,10 +10,67 @@
 
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12">
-                <p>Info cards</p>
+            <div class="col-12 mb-2" id="infoBar">
+                <div class="row">
+                    <div class="col-6 col-lg-3">
+
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>150</h3>
+                                <p>New Orders</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fas fa-arrow-circle-right"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-6 col-lg-3">
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <h3>53<sup style="font-size: 20px">%</sup></h3>
+                                <p>Bounce Rate</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-stats-bars"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-6 col-lg-3">
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3>44</h3>
+                                <p>User Registrations</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-person-add"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-6 col-lg-3">
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>65</h3>
+                                <p>Unique Visitors</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-pie-graph"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-12">
+
+            <div class="col-12 mb-2" id="addBar">
                 <form class="form-inline">
                     <div class="form-group mx-sm-3 mb-2">
                         <label for="links" class="sr-only">Links</label>
@@ -33,6 +90,38 @@
                     </div>
                     <button type="buttom" class="btn btn-success mb-2">Save</button>
                 </form>
+            </div>
+            <div class="col-12 mb-2 mr-2" id="optionBar">
+                <select name="year" id="year" class="col-3 float-right form-control">
+                    <option value="">2016</option>
+                    <option value="">2017</option>
+                    <option value="">2018</option>
+                    <option value="">2019</option>
+                </select>
+                <select name="" id="" class="col-3 float-right form-control">
+                    <option value="">march</option>
+                </select>
+            </div>
+            <div class="col-12 mb-2" id="informationTable">
+                <table class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">Links</th>
+                            <th scope="col">Referals</th>
+                            <th scope="col">Pop Ads</th>
+                            <th scope="col">Others</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>Otto</td>
+                            <td>Otto</td>
+                        </tr>
+
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
