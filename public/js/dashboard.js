fafc1453a9ad225a271d07df510c05b04321fde5
Kevin <kevin.cano.pc@gmail.com>
2023-01-30 14:04:46 -0600

    UPADTE: JS has ben success

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -20,31 +20,26 @@ function add() {
     };
 
     $.ajax({
-        url: "dashboard/request",
-        method: "POST",
-        data: params,
-        beforeSend: function () {},
-        statusCode: {
-            404: () => {
-                msgSweetAlert("404");
+            url: "dashboard/request",
+            method: "POST",
+            data: params,
+            beforeSend: function () {},
+            statusCode: {
+                404: () => {
+                    msgSweetAlert("404");
+                },
+                500: () => {
+                    msgSweetAlert("500");
+                },
             },
-            500: () => {
-                msgSweetAlert("500");
-            },
-        },
-    })
+        })
         .done(function (response) {
-            console.log('response', response);
+            console.log('responseeeeee', response);
             if (response.success) {
                 $("#newUserModal").modal("hide");
                 msgSweetAlert("success");
 
-                /*
-
-                vamos a estrar los datos del nuevo collection y los vamos a agregar o ya veremos algo que sea rapido 
-                
-                */
-
+                createElement('create', response.name, response.user_id, description, amount, response.itemId);
 
             } else {
                 msgSweetAlert("error");
@@ -52,6 +47,207 @@ function add() {
         })
         .then(() => {});
 }
+
+
+function createElement(type, name, userId, description, amount, itemId) {
+    const payDataDivRow = document.querySelector('#payDataDivRow');
+    const paidDataDivRow = document.querySelector('#paidDataDivRow');
+    
+    let response = `
+    <div class="col-12 col-sm-12 col-md-4 col-lg-3" id="element-${itemId}">
+        <div class="card">
+            <div class="card-header">
+                <b>${name}</b>
+                <label class="float-right"><i class="fa fa-eye showUser" aria-hidden="true"
+                        id="showUser-${userId}"
+                        user="${userId}"></i></label>
+            </div>
+            <div class="card-body">
+                <p class="card-text text-muted">${description}</p>
+                <hr />
+                    <p class="card-text text-danger" id="amount-${itemId}">
+                        <b>${amount}</b></p>
+            </div>`;
+            if (type == 'create'){
+                response += `<a href="javascript:void(0)" class="card-link bg-success paynow" id="a-${itemId}">`;
+            }else if (type == 'update'){
+                response += `<a href="javascript:void(0)" class="card-link bg-secondary" disabled="true" style="pointer-events: none;">`;
+            }
+            response += `
+                <div class="card-footer text-center" collection="${itemId}"
+                id="collection-${itemId}">Pay</div>
+            </a>
+        </div>
+    </div>`;
+    if (type == 'create'){
+        payDataDivRow.innerHTML = response + payDataDivRow.innerHTML;
+
+    }else if (type == 'update'){
+        paidDataDivRow.innerHTML = response + paidDataDivRow.innerHTML;
+
+    }
+
+    getItemsEvent();
+
+    return;
+}
+
+function getItemsEvent(){
+    
+    /*This is for every button pay for show a sweetAlert question*/
+    let payItems = document.querySelectorAll(".paynow");
+    payItems.forEach((element) => element.addEventListener("click", pay));
+
+    /*This is for every button show for show modal*/
+    const dashboardItems = document.querySelectorAll(".showUser");
+    dashboardItems.forEach((element) => element.addEventListener("click", show));
+
+
+
+}
+
+function show(value) {
+    const user_id = value.target.getAttribute("user");
+    const params = {
+        type: "get",
+        user_id: user_id,
+        _token: csrf
+    };
+    userCollectionsTable.innerHTML = '';
+
+    $.ajax({
+            url: "dashboard/request",
+            method: "POST",
+            data: params,
+            beforeSend: function () {},
+            statusCode: {
+                404: () => {
+                    msgSweetAlert("404");
+                },
+                500: () => {
+                    msgSweetAlert("500");
+                },
+            },
+        })
+        .done(function (response) {
+            if (response.success) {
+                const items = response.items.reverse();
+                items.forEach((element) => {
+                    const tr = document.createElement("tr");
+                    const tdAmount = document.createElement("td");
+                    const tdPay = document.createElement("td");
+                    const tdCreated = document.createElement("td");
+                    const tdPayment = document.createElement("td");
+                    const amountLocal = element.amount;
+                    const currency = function (number) {
+                        return new Intl.NumberFormat("en-us", {
+                            style: "currency",
+                            currency: "USD",
+                            minimumFractionDigits: 2,
+                        }).format(number);
+                    };
+                    tdAmount.innerText = currency(amountLocal);
+                    element.pay ?
+                        tr.classList.add("table-success") :
+                        tr.classList.add("table-danger");
+                    tdPay.innerText = element.pay ? "Yes" : "No";
+                    tdCreated.innerText = new Date(
+                        element.created_at
+                    ).toLocaleDateString();
+                    tdPayment.innerText = new Date(
+                        element.updated_at
+                    ).toLocaleDateString();
+                    const al = new Date(element.updated_at);
+
+                    tr.appendChild(tdAmount);
+                    tr.appendChild(tdPay);
+                    tr.appendChild(tdCreated);
+                    tr.appendChild(tdPayment);
+                    userCollectionsTable.appendChild(tr);
+                    $("#showCardModal").modal("show");
+                });
+            } else {
+                msgSweetAlert("error");
+            }
+        })
+        .then(() => {});
+}
+
+function pay(value) {
+    console.log('value', value);
+    let collection;
+
+    if (typeof value === 'number') {
+        collection = value;
+
+    } else {
+        collection = value.target.getAttribute("collection");
+    }
+
+    console.log('collection', collection);
+
+
+    const params = {
+        type: "update",
+        collection: collection,
+        _token: csrf
+    };
+
+    Swal.fire({
+        title: "Are you sure?",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Yes",
+        denyButtonText: "No",
+        customClass: {
+            actions: "my-actions",
+            cancelButton: "order-1 right-gap",
+            confirmButton: "order-2",
+            denyButton: "order-3",
+        },
+    }).then((result) => {
+        if (result.value) {
+            $.ajax({
+                    url: "dashboard/request",
+                    method: "POST",
+                    data: params,
+                    beforeSend: function () {},
+                    statusCode: {
+                        404: () => {
+                            msgSweetAlert("404");
+                        },
+                        500: () => {
+                            msgSweetAlert("500");
+                        },
+                    },
+                })
+                .done(function (response) {
+                    console.log('response', response);
+                    if (response.success) {
+
+
+                        const thisItem = document.querySelector(`#element-${collection}`);
+                        thisItem.style.display = 'none';
+                        createElement('update', response.name, response.user_id, response.description, response.amount, response.itemId);
+
+                        // const thisElementId = value.target.id;
+                        // document.querySelector(`#${thisElementId}`).classList.add('bg-secondary');
+                        // document.querySelector(`#a-${collection}`).classList.remove('paynow');
+                        // document.querySelector(`#a-${collection}`).style = "pointer-events: none;";
+                        // document.querySelector(`#amount-${collection}`).classList.remove("text-danger");
+                        // document.querySelector(`#amount-${collection}`).classList.add("text-success");
+
+                        /*thisElementId is id for every collection and next we add a class bg-secondary for change color and block her pinter-events and remove paynow class for the "a" element and change text-danger for text-success in the amount*/
+                        msgSweetAlert("success");
+                    } else {
+                        msgSweetAlert("error");
+                    }
+                })
+                .then(() => {});
+        }
+    });
+}
+
 function msgSweetAlert(value) {
     switch (value) {
         case "500":
@@ -93,164 +289,5 @@ function msgSweetAlert(value) {
     }
 }
 
-function show(value) {
-    const user_id = value.target.getAttribute("user");
-    const params = { type: "get", user_id: user_id, _token: csrf };
-    userCollectionsTable.innerHTML = '';
-
-    $.ajax({
-        url: "dashboard/request",
-        method: "POST",
-        data: params,
-        beforeSend: function () {},
-        statusCode: {
-            404: () => {
-                msgSweetAlert("404");
-            },
-            500: () => {
-                msgSweetAlert("500");
-            },
-        },
-    })
-        .done(function (response) {
-            if (response.success) {
-                const items = response.items.reverse();
-                items.forEach((element) => {
-                    const tr = document.createElement("tr");
-                    const tdAmount = document.createElement("td");
-                    const tdPay = document.createElement("td");
-                    const tdCreated = document.createElement("td");
-                    const tdPayment = document.createElement("td");
-                    const amountLocal = element.amount;
-                    const currency = function (number) {
-                        return new Intl.NumberFormat("en-us", {
-                            style: "currency",
-                            currency: "USD",
-                            minimumFractionDigits: 2,
-                        }).format(number);
-                    };
-                    tdAmount.innerText = currency(amountLocal);
-                    element.pay
-                        ? tr.classList.add("table-success")
-                        : tr.classList.add("table-danger");
-                    tdPay.innerText = element.pay ? "Yes" : "No";
-                    tdCreated.innerText = new Date(
-                        element.created_at
-                    ).toLocaleDateString();
-                    tdPayment.innerText = new Date(
-                        element.updated_at
-                    ).toLocaleDateString();
-                    const al = new Date(element.updated_at);
-
-                    tr.appendChild(tdAmount);
-                    tr.appendChild(tdPay);
-                    tr.appendChild(tdCreated);
-                    tr.appendChild(tdPayment);
-                    userCollectionsTable.appendChild(tr);
-                    $("#showCardModal").modal("show");
-                });
-            } else {
-                msgSweetAlert("error");
-            }
-        })
-        .then(() => {});
-}
-
-function pay(value) {
-    let collection;
-
-    if(typeof value === 'number'){
-        collection = value;
-
-    }else{
-        collection = value.target.getAttribute("collection");
-    }
-
-
-    const params = { type: "update", collection: collection, _token: csrf };
-
-    Swal.fire({
-        title: "Are you sure?",
-        showDenyButton: true,
-        showCancelButton: true,
-        confirmButtonText: "Yes",
-        denyButtonText: "No",
-        customClass: {
-            actions: "my-actions",
-            cancelButton: "order-1 right-gap",
-            confirmButton: "order-2",
-            denyButton: "order-3",
-        },
-    }).then((result) => {
-        if (result.value) {
-            $.ajax({
-                url: "dashboard/request",
-                method: "POST",
-                data: params,
-                beforeSend: function () {},
-                statusCode: {
-                    404: () => {
-                        msgSweetAlert("404");
-                    },
-                    500: () => {
-                        msgSweetAlert("500");
-                    },
-                },
-            })
-                .done(function (response) {
-                    console.log('response', response);
-                    if (response.success) {
-                        // const thisElementId = value.target.id;
-                        // document.querySelector(`#${thisElementId}`).classList.add('bg-secondary');
-                        // document.querySelector(`#a-${collection}`).classList.remove('paynow');
-                        // document.querySelector(`#a-${collection}`).style = "pointer-events: none;";
-                        // document.querySelector(`#amount-${collection}`).classList.remove("text-danger");
-                        // document.querySelector(`#amount-${collection}`).classList.add("text-success");
-                        
-                        /*thisElementId is id for every collection and next we add a class bg-secondary for change color and block her pinter-events and remove paynow class for the "a" element and change text-danger for text-success in the amount*/
-                        msgSweetAlert("success");
-                    } else {
-                        msgSweetAlert("error");
-                    }
-                })
-                .then(() => {});
-        }
-    });
-}
-
-
-
-
-
-/*This is for every button pay for show a sweetAlert question*/
-let payItems = document.querySelectorAll(".paynow");
-payItems.forEach((element) => element.addEventListener("click", pay));
-
-/*This is for every button show for show modal*/
-const dashboardItems = document.querySelectorAll(".showUser");
-dashboardItems.forEach((element) => element.addEventListener("click", show));
-
+getItemsEvent();
 addBtn.addEventListener("click", add);
-
-
-/*
-
-
-let payItems = document.querySelectorAll(".paynow");
-payItems.forEach((element) => element.addEventListener("click", pay));
-
-const dashboardItems = document.querySelectorAll(".showUser");
-dashboardItems.forEach((element) => element.addEventListener("click", show));
-
-addBtn.addEventListener("click", add);
-
-
-
-
-Esto hay que revisarlo ya que en el otro lado tenemos que usar 
-        collection = value.target.getAttribute("collection");
-
-        esetarget no me gusta por lo que tengo que investigar bien bien como está funcionando ya que en el html tendría que agregar el attribute especificamente en el dato que mandamos a traer desde querySelecectorAll
-
-
-*/
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -13,40 +13,88 @@
             <div class="col-12" id="divBtnNewUser">
                 <button class="btn btn-primary float-right" data-toggle="modal" data-target="#newUserModal">New User</button>
             </div>
-            {{-- {{dd($data)}} --}}
-            @if (count($data) < 0)
-                <h1>No items to display yet.</h1>
-            @endif
-            @foreach ($data as $element)
-                <div class="col-12 col-sm-12 col-md-4 col-lg-3">
-                    <div class="card">
-                        <div class="card-header">
-                            <b>{{ $element->username->name }}</b>
-                            {{-- <label class="float-right" data-toggle="modal" data-target="#showCardModal"><i class="fa fa-eye" --}}
-                                <label class="float-right"><i class="fa fa-eye showUser"
-                                    aria-hidden="true" id="showUser-{{$element->user_id}}" user="{{$element->user_id}}"></i></label>
-                        </div>
-                        <div class="card-body">
-                            <p class="card-text text-muted">{{ $element->description }}</p>
-                            <hr />
-                            @if ($element->pay == 0)
-                                <p class="card-text text-danger" id="amount-{{$element->id}}"><b>${{ $element->amount }}</b></p>
-                            @else
-                                <p class="card-text text-success"><b>${{ $element->amount }}</b></p>
-                            @endif
-                        </div>
-                        @if ($element->pay == 0)
-                            <a href="javascript:void(0)" class="card-link bg-success paynow" id="a-{{$element->id}}">
-                            @else
-                                <a href="javascript:void(0)" class="card-link bg-secondary" disabled="true"
-                                    style="pointer-events: none;">
-                        @endif
-                        <div class="card-footer text-center" collection="{{$element->id}}" id="collection-{{$element->id}}">Pay</div>
-                        </a>
-                    </div>
+            <div class="col-12 mb-3" id="payDataDiv">
+                <div class="row" id="payDataDivRow">
+                    {{-- {{dd($payData)}} --}}
+                    @if (count($payData) < 0)
+                        <h1>No items to display yet.</h1>
+                    @endif
+                    @foreach ($payData as $element)
+                        <div class="col-12 col-sm-12 col-md-4 col-lg-3" id="element-{{ $element->id }}">
+                            <div class="card">
+                                <div class="card-header">
+                                    <b>{{ $element->username->name }}</b>
+                                    {{-- <label class="float-right" data-toggle="modal" data-target="#showCardModal"><i class="fa fa-eye" --}}
+                                    <label class="float-right"><i class="fa fa-eye showUser" aria-hidden="true"
+                                            id="showUser-{{ $element->user_id }}"
+                                            user="{{ $element->user_id }}"></i></label>
+                                </div>
+                                <div class="card-body">
+                                    <p class="card-text text-muted">{{ $element->description }}</p>
+                                    <hr />
+                                    @if ($element->pay == 0)
+                                        <p class="card-text text-danger" id="amount-{{ $element->id }}">
+                                            <b>${{ $element->amount }}</b></p>
+                                    @else
+                                        <p class="card-text text-success"><b>${{ $element->amount }}</b></p>
+                                    @endif
+                                </div>
+                                @if ($element->pay == 0)
+                                    <a href="javascript:void(0)" class="card-link bg-success paynow"
+                                        id="a-{{ $element->id }}">
+                                    @else
+                                        <a href="javascript:void(0)" class="card-link bg-secondary" disabled="true"
+                                            style="pointer-events: none;">
+                                @endif
+                                <div class="card-footer text-center" collection="{{ $element->id }}"
+                                    id="collection-{{ $element->id }}">Pay</div>
+                                </a>
+                            </div>
 
+                        </div>
+                    @endforeach
                 </div>
-            @endforeach
+                <hr>
+
+            </div>
+            <div class="col-12 pt-1" id="paidDataDiv">
+                <div class="row"  id="paidDataDivRow">
+                    @foreach ($paidData as $element)
+                        <div class="col-12 col-sm-12 col-md-4 col-lg-3" id="element-${{ $element->id }}">
+                            <div class="card">
+                                <div class="card-header">
+                                    <b>{{ $element->username->name }}</b>
+                                    {{-- <label class="float-right" data-toggle="modal" data-target="#showCardModal"><i class="fa fa-eye" --}}
+                                    <label class="float-right"><i class="fa fa-eye showUser" aria-hidden="true"
+                                            id="showUser-{{ $element->user_id }}"
+                                            user="{{ $element->user_id }}"></i></label>
+                                </div>
+                                <div class="card-body">
+                                    <p class="card-text text-muted">{{ $element->description }}</p>
+                                    <hr />
+                                    @if ($element->pay == 0)
+                                        <p class="card-text text-danger" id="amount-{{ $element->id }}">
+                                            <b>${{ $element->amount }}</b></p>
+                                    @else
+                                        <p class="card-text text-success"><b>${{ $element->amount }}</b></p>
+                                    @endif
+                                </div>
+                                @if ($element->pay == 0)
+                                    <a href="javascript:void(0)" class="card-link bg-success paynow"
+                                        id="a-{{ $element->id }}">
+                                    @else
+                                        <a href="javascript:void(0)" class="card-link bg-secondary" disabled="true"
+                                            style="pointer-events: none;">
+                                @endif
+                                <div class="card-footer text-center" collection="{{ $element->id }}"
+                                    id="collection-{{ $element->id }}">Pay</div>
+                                </a>
+                            </div>
+
+                        </div>
+                    @endforeach
+                </div>
+            </div>
         </div>
     </div>
 
@@ -121,7 +169,7 @@
                         </div>
                         <div class="form-group">
                             <label for="description">Description</label>
-                            <textarea name="description" id="description" rows="5" class="form-control"></textarea>
+                            <textarea name="description" id="description" rows="5" class="form-control">dda</textarea>
                         </div>
                         <div class="form-row">
                             {{-- <div class="form-group col-6">
@@ -133,7 +181,7 @@
                             </div> --}}
                             <div class="form-group col-6">
                                 <label for="date_info">Date</label>
-                                <input type="date" name="date_info" id="date_info" class="form-control">
+                                <input type="date" name="date_info" id="date_info" class="form-control" value="2023-01-03">
                             </div>
                             <div class="form-group col-6">
                                 <label for="time_type">Range</label>
@@ -148,7 +196,7 @@
                         </div>
                         <div class="form-group">
                             <label for="amount">Amount</label>
-                            <input type="number" name="amount" id="amount" class="form-control">
+                            <input type="number" name="amount" id="amount" class="form-control" value="30">
                         </div>
                         {{-- <div class="form-group form-check">
                             <input type="checkbox" class="form-check-input" id="extends_data">
