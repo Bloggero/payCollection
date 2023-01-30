498d2aaddecbfd05ddd2245655d2d1f51ae148ee
Kevin <kevin.cano.pc@gmail.com>
2023-01-30 12:33:51 -0600

    DELETE: Last data of statistics

--- routes/admin.php
+++ routes/admin.php
@@ -12,8 +12,3 @@ Route::get('/', function () {
 Route::get('/dashboard', [CollectionController::class, 'index']);
 
 Route::post('/dashboard/request', [CollectionController::class, 'index']);
-
-
-Route::get('/statistics', [StatisticController::class, 'index']);
-Route::post('/statistics/request', [StatisticController::class, 'index'])->name('statisticsRequest');
-
